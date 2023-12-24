ffbf9f21082a0dc074b61ec9bf4b6d8f2e23a72f
Tuba <uzayussu@Tuba-MacBook-Pro.local>
2023-12-24 14:50:23 +0300

    revised code

--- selenyum-start/Test_SauceDemo.py
+++ selenyum-start/Test_SauceDemo.py
@@ -7,39 +7,39 @@ from selenium.webdriver.support import expected_conditions  as ec #beklenen koş
 from selenium.webdriver.common.action_chains import ActionChains
 
 class Test_SauceDemo:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://www.saucedemo.com")
+        self.driver.maximize_window()  
+        
+        
     def test_invalid_login(self):
-        driver = webdriver.Chrome()
-        driver.get("http://www.saucedemo.com")
-        driver.maximize_window()  
-
-        usernameInput = WebDriverWait(driver,5).until(ec.visibility_of_element_located((By.ID, "user-name")))
-        #usernameInput = driver.find_element(By.ID, "user-name")
+        self.driver.get("http://www.saucedemo.com")
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, "user-name")))
         usernameInput.send_keys("locked_out_user")
         
-        passwordInput = WebDriverWait(driver,5).until(ec.visibility_of_element_located((By.ID, "password")))
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, "password")))
         passwordInput.send_keys("secret_sauce")
        
-        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton = self.driver.find_element(By.ID,"login-button")
         loginButton.click()
         
-        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
         testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
         print(f"TEST SONUCU: {testResult}")
 
     def valid_login(self):
-        driver = webdriver.Chrome()
-        driver.get("http://www.saucedemo.com")
-        driver.maximize_window()  
+        self.driver.get("http://www.saucedemo.com")
 
-        usernameInput = WebDriverWait(driver,5).until(ec.visibility_of_element_located((By.ID, "user-name")))
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, "user-name")))
         usernameInput.send_keys("standart_user")
         
-        passwordInput = WebDriverWait(driver,5).until(ec.visibility_of_element_located((By.ID, "password")))
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, "password")))
         passwordInput.send_keys("secret_sauce")
        
-        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton = self.driver.find_element(By.ID,"login-button")
         loginButton.click()
-        headerLogo = WebDriverWait(driver,5).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='header_container']/div[1]/div[2]/div")))
+        headerLogo = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='header_container']/div[1]/div[2]/div")))
         testResult = header.Logo.text == "Swag Labs"
         print(f"TEST SONUCU: {testResult}")
 
